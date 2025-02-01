ed4be6fdbcbcc44786d91a45589c2644214d6465
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-02-01 17:44:33 +0500

    Update 1672. Richest Customer Wealth.py

--- 1672. Richest Customer Wealth.py
+++ 1672. Richest Customer Wealth.py
@@ -1,1 +1,8 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        return max(map(sum,accounts))
+        
 
+    # Time complexity O(m*n)    
+    
+    #  Space complexity O(m)
