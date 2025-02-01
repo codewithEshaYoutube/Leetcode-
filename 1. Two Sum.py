bb6695dd2740604fcf6aa7246e1a09aa69c1dcb1
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-02-01 17:48:21 +0500

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -1,1 +1,15 @@
+class Solution:
+    def twoSum(self, nums, target):
+        Esha = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in Esha:
+                return [Esha[complement],i]
 
+            Esha[num] = i 
+        return []
+       
+ 
+
+
+       
