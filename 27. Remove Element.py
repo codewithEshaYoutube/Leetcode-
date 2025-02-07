e250b182d227481121a6f7cde97d7ad68d0c5541
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-02-07 22:09:19 +0500

    Update 27. Remove Element.py

--- 27. Remove Element.py
+++ 27. Remove Element.py
@@ -1,1 +1,11 @@
+class Solution:
+    def removeElement(self, nums, val):  # Add 'self' for instance method
+        i = 0  # Pointer to track placement of non-val elements
+
+        for x in nums:
+            if x != val:  
+                nums[i] = x  # Move non-val element to position i
+                i += 1  # Move pointer forward
+
+        return i  # Return new length (number of non-val elements)
 
