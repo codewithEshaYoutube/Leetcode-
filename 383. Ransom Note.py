48b4e4107eb39c232060e9017e69526934d6a656
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-03-07 22:29:46 +0500

    Update 383. Ransom Note.py

--- 383. Ransom Note.py
+++ 383. Ransom Note.py
@@ -1,1 +1,14 @@
+from collections import Counter
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        magazine_count = Counter(magazine)
+
+        # Directly check and decrement magazine_count
+        for char in ransomNote:
+            if magazine_count[char] == 0:
+                return False
+            magazine_count[char] -= 1
+        
+        return True
 
