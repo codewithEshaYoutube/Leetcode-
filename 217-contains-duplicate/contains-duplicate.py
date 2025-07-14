36af76902e19ca5fbd97db3091988de91f05ea52
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-07-14 11:10:49 +0500

    Time: 13 ms (62.90%) | Memory: 31.5 MB (82.44%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -1,13 +1,24 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        """
         #using hasmap 
-        num_map={}
-        for n in nums:
-            if n in num_map:
-                return True # found duplicate
-            num_map[n]= 1
-        return False
+            num_map={}
+            for n in nums:
+                if n in num_map:
+                    return True # found duplicate
+                num_map[n]= 1
+            return False
         #T:O(n) S:O(n)
+        """
+        #using hash set
+        seen= set()
+
+        for n in nums:
+            if n in seen:
+                return True # found duplicate
+            seen.add(n)
+        return False
+        
                 
             
         
