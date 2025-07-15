f0816f26e3683c72dca813e7df25600a79fc7cb4
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-07-15 11:13:20 +0500

    Time: 1 ms (39.83%) | Memory: 18.1 MB (24.19%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -1,11 +1,20 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        common_set=set()
-        for i in nums1:
-            for j in nums2:
-                if i==j:
-                    common_set.add(i)
+        # common_set=set()
+        # for i in nums1:
+        #     for j in nums2:
+        #         if i==j:
+        #             common_set.add(i)   #T On*m
                 
-        return list(common_set)
+        # return list(common_set)
+        """ solution 2"""
+        # return list(set(nums1) & set(nums2))
+        # T=O(n+m)
+        # S=O(n+m)
+        """ solution 3"""
+        set1=set(nums1)
+        set2=set(nums2)
+        return list(set1 & set2)
+
 
         
