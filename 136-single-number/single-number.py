9f84d06cac3cae1497b9c0d607e824631bc3badb
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-08-20 12:53:41 +0500

    Time: 7 ms (32.53%) | Memory: 19.8 MB (21.13%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,34 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        """
-        S:O(1)
-        T:O(n)
-        1,set
-        2, make a loop 
-        3.check the duplicate
-        4.we can emoinate the duplicates
-        5. return array
-        """
-        """
-        nums_set=set()
+        freq={}
         for n in nums:
-            if n in nums_set:
-                nums_set.remove(n)
-            else:
-                nums_set.add(n)
-        return nums_set.pop()
-        """
-        exclusive=0
-        for n in nums:
-            exclusive^=n
-        return exclusive
-        """
-        exclusive or
-        T:linear time o(n)
-        S: constant O(1)
-        """
-
-        
-                
-
-        
+            freq[n]=freq.get(n,0)+1
+        for n in freq:
+            if freq[n]==1:
+                return n
+            
