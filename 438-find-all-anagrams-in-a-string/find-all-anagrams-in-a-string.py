dff2936d5ba067e404dbdb9a14e9fbf39f83b44c
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-08-22 23:11:18 +0500

    Time: 39 ms (73.73%) | Memory: 18.3 MB (78.78%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.py
@@ -1,5 +1,23 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
+        """
+        Approach:
+        1. Create frequency dictionaries for p and the first window of s with size len(p).
+        2. Compare both dictionaries:
+            - If equal, add index 0 to result.
+        3. Slide the window one step at a time:
+            - Add the new character from the right.
+            - Remove the old character from the left.
+            - Update the hashmap counts.
+            - If a character’s count becomes zero, remove it from hashmap to keep dictionary sizes small.
+            - If updated hashmap matches p’s hashmap, record the starting index.
+        4. Return the list of starting indices.
+
+        Complexity:
+        - Time complexity: O(n), where n = len(s), because each character is processed once when entering and once when leaving the window.
+        - Space complexity: O(1), since the maximum hashmap size is bounded by 26 letters (constant space).
+ 
+        """
         #hashmap
         if len(s)<len(p):
             return[]
