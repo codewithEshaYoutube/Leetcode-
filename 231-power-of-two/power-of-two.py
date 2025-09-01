56405b15c8178cef86e6dd55d45545cae88f6793
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-09-01 11:56:07 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (83.73%) - LeetSync

---
+++ 231-power-of-two/power-of-two.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        def power(x):
+            # base case
+            if x==1:
+                return True
+            # recursiv ecas
+            if x<=0 or x%2!=0:
+                return False
+            return (power(x//2))
+        return power(n)
+            
