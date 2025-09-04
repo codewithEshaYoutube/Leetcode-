31159734eae6c0fa67148160e6b231277266eb20
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-09-04 12:02:23 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (58.64%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,11 +1,19 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
+        """
+        res=[[1]]
+        two pointer
+        temp  each row we will store
+        pointer update
+        res.append
+        we will return
+        
+        """
         res=[[1]]
         for i in range(numRows-1):
             temp=[0]+res[-1]+[0]
             row=[]
             for j in range(len(res[-1])+1):
-                row.append(temp[j]+temp[j+1])
+                row.append(temp[j]+temp[j+1])# adding values
             res.append(row)
         return res
-        
