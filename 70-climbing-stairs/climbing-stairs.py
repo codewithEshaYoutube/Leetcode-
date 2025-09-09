e889ae4a5c079fbcb632b50956204a38bcf943c0
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-09-09 23:41:27 +0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (78.38%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,8 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one,two=1,1
+        """
+        (n-1) 
+        two variables  one, two
+        keep on updating then 
+        return one
+        """
+        one=1
+        two=1
         for i in range(n-1):
             temp=one
             one=one+two
             two=temp
         return one
+        
