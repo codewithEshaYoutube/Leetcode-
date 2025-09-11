8794ad470d571a95edecb5c75bb3e9494d7d77c0
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-09-12 00:00:23 +0500

    Time: 699 ms (82.93%) | Memory: 18 MB (87.64%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,13 +1,24 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        # Initialize dp with infinity
-        dp = [float("inf")] * (amount + 1)
-        dp[0] = 0   # base case
+        """
+        bottomm up approach
+        dp array  inf
+        base case 0 
+        fill array for each coin
+        looping over coins
+        min ()
+        return combination
         
-        # Fill dp array
+        """
+        dp=[float("inf")] * (amount+1)
+        dp[0]=0 # base case
+        # filling and updating array
         for coin in coins:
-            for x in range(coin, amount + 1):
-                dp[x] = min(dp[x], dp[x - coin] + 1)
+            for x in range(coin,amount+1):
+                dp[x]=min(dp[x],dp[x-coin]+1)
+
+        return dp[amount] if dp[amount]!=float("inf") else -1
+
         
-        return dp[amount] if dp[amount] != float("inf") else -1
-            
+            # T :O(coin*amount)
+            #S : O(amount)
