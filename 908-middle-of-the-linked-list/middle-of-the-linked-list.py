d14f84202c20f4578ead0e7ddace171d28ad9b3c
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-10-21 00:37:05 +0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (79.90%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -1,24 +1,14 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        """
-        setting up positions for fast and slow
-        loop (fast and fast.next)
-        slow move one time
-        fast  move two time
-        return slow
-        """
-        slow,fast=head,head
-        while fast and fast.next:
-            slow=slow.next
-            fast=fast.next.next
-        return slow
-        # T : O(n)
-        #S:O(1)
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-        
-        
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        slow = fast = head
+        # Move fast by 2 steps and slow by 1 step
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
