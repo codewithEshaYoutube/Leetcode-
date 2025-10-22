84fa4c9bd63633386111a44eb05f7493a967a191
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-10-23 01:21:02 +0500

    Time: 11 ms (58.69%) | Memory: 20.4 MB (17.25%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.py
@@ -8,22 +8,19 @@ import heapq
 
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        heap = []
-        
-        # Initialize the heap with the first node of each list
+        heap=[]
+        dummy=ListNode()
+        curr=dummy
         for i, node in enumerate(lists):
             if node:
-                heapq.heappush(heap, (node.val, i, node))
-        
-        dummy = ListNode(0)
-        curr = dummy
-        
-        # Extract the smallest node and add its next node to heap
+                heapq.heappush(heap,(node.val,i,node))
         while heap:
-            val, i, node = heapq.heappop(heap)
-            curr.next = node
-            curr = curr.next
-            if node.next:
-                heapq.heappush(heap, (node.next.val, i, node.next))
-        
+            val,i,node=heapq.heappop(heap)
+            curr.next=node
+            curr=node
+            node=node.next
+            if node:
+                heapq.heappush(heap,(node.val,i,node))
+
         return dummy.next
+        
