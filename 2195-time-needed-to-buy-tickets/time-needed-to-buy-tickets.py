08ed0e66e9f0c614456c215d2749d32a9513ae8b
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-10-28 07:49:40 +0500

    Time: 7 ms (53.52%) | Memory: 17.9 MB (36.12%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -1,9 +1,17 @@
+from collections import deque
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        q=deque([(i,t) for i,t in enumerate(tickets)])
         time=0
-        for i in range(len(tickets)):
-            if i<=k:
-                time+=min(tickets[i],tickets[k])
-            else:
-                time+=min(tickets[i],tickets[k]-1)
-        return time
+        while q:
+            i,t=q.popleft()
+            t-=1
+            time+=1
+            if t>0:
+                q.append((i,t))
+            if i==k and t==0:
+                return time
+
+
+       
+        
