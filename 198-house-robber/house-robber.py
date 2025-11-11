a69aa775006eb229c76687f85897988e574d8314
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-11-11 08:04:30 +0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.71%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,24 +1,33 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         """
-        edge cases
-        dp array
-        loop
-        skipping and choosing maximum
+        bottom up tabulation
+        base cases 
+        looping over n
+        skipping the house ,choosing max
         return max
-        
+
         """
         n=len(nums)
-        #edge cases
         if n==0:
-            return 0
+            return n
         if n==1:
             return nums[0]
-        
-        # dynamic programming array
-        dp=[0]* n
+        dp=[0]*n
         dp[0]=nums[0]
-        dp[1]=max(nums[0],nums[1]) # 2 houses
+        dp[1]=max(nums[0],nums[1])
         for i in range(2,n):
             dp[i]=max(nums[i]+dp[i-2],dp[i-1])
         return dp[n-1]
+
+
+
+
+
+        
+
+
+
+
+
+
