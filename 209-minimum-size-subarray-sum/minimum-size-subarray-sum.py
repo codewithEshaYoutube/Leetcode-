3e2e0b806e30b001ab29b1c061adb3082280f270
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-12-04 23:52:10 +0500

    Time: 7 ms (92.33%) | Memory: 28.4 MB (10.83%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,15 +1,30 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        left = 0
-        window_sum = 0
-        res = float("inf")
+        """
+        min_length=  (inf)
+        curr_sum  initailze
+        left is intialzed on 0th indexed
+        looping over  length
+        expansion of window until target
+        desicion  if target achieved 
+        save the no. of combination ( min )
+        shrinking
+        return min length of subarray
+        
+        """
+        min_length=float('inf')
+        curr_sum=0
+        left=0
+        n=len(nums)
+        for right in range (n):
+            curr_sum+=nums[right]
+            while curr_sum>=target:
+                min_length=min(min_length,right-left+1) # window size 
+                curr_sum-=nums[left]
+                left+=1
+        return min_length if min_length < float('inf') else 0
 
-        for right in range(len(nums)):
-            window_sum += nums[right]
 
-            while window_sum >= target:
-                res = min(res, right - left + 1)
-                window_sum -= nums[left]
-                left += 1
 
-        return 0 if res == float("inf") else res
+
+       
