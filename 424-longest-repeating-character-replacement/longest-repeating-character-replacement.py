02008c1dc7431d0950f671e1622cb5bb7b40c34f
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2025-12-05 23:59:06 +0500

    Time: 116 ms (65.33%) | Memory: 18.2 MB (22.99%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,23 +1,37 @@
 from collections import defaultdict
-
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         
-        count = defaultdict(int)   # character frequency in window
-        left = 0
-        max_count = 0              # most frequent character count in window
-        result = 0
+        """
+        variable sliding window
+        left , right pointer   window 
+        res, max_count
+        loop
+        expansion
+        storing the max
+        conditions met  (k no. of changings)  next char is not like prev
+        shrink
+        storing the longest
+        return res
 
-        for right in range(len(s)):
-            count[s[right]] += 1
-            max_count = max(max_count, count[s[right]])
 
-            # if window is invalid → shrink from left
-            while (right - left + 1) - max_count > k:
-                count[s[left]] -= 1
-                left += 1
 
-            # update longest valid window size
-            result = max(result, right - left + 1)
-
+        """
+        count=defaultdict(int)
+        left=0
+        max_count=0
+        result=0
+        n=len(s)
+        for right in range(n):
+            count[s[right]]+=1
+            max_count=max(max_count, count[s[right]])
+            # invalid 
+            while (right-left+1)-max_count>k:
+                count[s[left]]-=1
+                left+=1
+            result=max(result,right-left+1)
         return result
+
+
+
+        
