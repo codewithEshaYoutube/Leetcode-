10767e9c8b088967596d136b75817af0f9e645da
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2026-02-12 14:20:21 +0500

    Time: 47 ms (10.57%) | Memory: 32.1 MB (5.93%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        memo={}
+        
+        def dfs(i, j):
+            if i >= m or j >= n:
+                return float('inf')
+            
+            if i == m - 1 and j == n - 1:
+                return grid[i][j]
+            if (i,j)in memo:
+                return memo[(i,j)]
+            
+            right = dfs(i, j + 1)
+            left = dfs(i + 1, j)
+            
+            memo[(i,j)]= grid[i][j] + min(right, left)
+            return memo[(i,j)]
+        
+        return dfs(0, 0)
