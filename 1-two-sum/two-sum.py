fae736dda2c59c1d1a274962583b0d9cbf531553
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2026-03-04 00:07:06 +0500

    Time: 0 ms (100.00%) | Memory: 20.2 MB (61.19%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,8 +1,34 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-       hashmap={}
-       for i,num in enumerate(nums):
-        diff=target-num
-        if diff in hashmap:
-            return [hashmap[diff],i]
-        hashmap[num]=i
+        """
+        nums
+        --> brute force
+        each element + each other element
+        compare target
+        return index
+        --> array 
+        find out pair of element making target
+        update in array
+        --> hashmap
+        target-num=complement
+        9-2=7
+        9-7=2
+        9-11=-4
+        9-15=6
+        
+        """
+        hashmap={}
+        n=len(nums)
+        for i,num in enumerate (nums):
+            
+            complement=target-nums[i]
+            
+            if complement in hashmap:
+                return [hashmap[complement],i]
+            hashmap[num]=i
+
+
+        
+
+
+       
