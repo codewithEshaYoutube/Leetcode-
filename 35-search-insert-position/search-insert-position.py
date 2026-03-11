783ae92b2e70157a3d0857050b3d03cb00c53c95
Eesha Tariq <157880759+codewithEshaYoutube@users.noreply.github.com>
2026-03-11 12:30:52 +0500

    Time: 0 ms (100.00%) | Memory: 20.1 MB (41.69%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,20 +1,26 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        low=0
+        """
+        low and high pointer
+        loop
+        mid 
+        comparisions
+        return low or high
+        """
+        low =0
         high=len(nums)-1
         while low<=high:
-            mid=(low+high)//2
+            mid=(low+high)//2 #  index
             guess=nums[mid]
             if guess==target:
                 return mid
-            elif guess<target:
-                low=mid+1
-            else:
+            elif guess>target:
                 high=mid-1
-            
+            else:
+                low=mid+1
         return low
+        #T: O(logn)
+        #S : O(1)
             
-        
-        
-         
-        
+
+       
